0c18ef020585171262d71ed0f77002228ec7d5c1
amandachien@igs.com.tw <xxdzgfyxgs_90016>
2025-06-16 15:58:11 +0800

    加入M01P的GameUpdate.apk的源碼

---
+++ M01P_GameUpdater/app/src/main/java/com/example/m01p_gameupdater/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.m01p_gameupdater
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.m01p_gameupdater.ui.theme.M01P_GameUpdaterTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            M01P_GameUpdaterTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    M01P_GameUpdaterTheme {
+        Greeting("Android")
+    }
+}
